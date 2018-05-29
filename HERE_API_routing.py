1cf937726cc0f9a4994dd6ac4ae5a3107cac4313
eRoutes <janlukaspflaum@gmail.com>
2018-05-30 01:53:40 +0200

    complete renewed
    nearly ready for fe integration

--- HERE_API_routing.py
+++ HERE_API_routing.py
@@ -7,6 +7,11 @@ Created on Mon May 14 23:08:19 2018
 """
 import pandas as pd
 import requests
+import geopy.distance
+
+# 1. all neccessary functions are listed here: 
+
+# a geocoder that translates an address or given place from a string into coordinates
 
 def geocoder(place):
     r = requests.get('https://geocoder.cit.api.here.com/6.2/geocode.json?app_id=jccZtyShstzovgbVxAJn&app_code=5ezWDCQaAJYiXldHFRV6gA&searchtext=' + place)
@@ -16,197 +21,141 @@ def geocoder(place):
     coordinates = str(latitude) + ',' + str(longitude)
     return coordinates
 
-start = 'Alexanderplatz+Berlin'
-destination = 'Schlossplatz+Stuttgart'
-
-start_coord = geocoder(start)
-destination_coord = geocoder(destination)
+# an api request that returns a route between two given waypoints
 
 def here_route(start_coord, destination_coord):
     r = requests.get('https://route.cit.api.here.com/routing/7.2/calculateroute.json?app_id=jccZtyShstzovgbVxAJn&app_code=5ezWDCQaAJYiXldHFRV6gA&waypoint0='+ start_coord + '&waypoint1=' + destination_coord + '&mode=fastest;car;traffic:disabled')
     route = r.json()
     return route
 
-route = here_route(start_coord,destination_coord)
-
-whole_trip_length_in_km = (route['response']['route'][0]['summary']['distance'])/1000
-whole_trip_time_in_h = round((route['response']['route'][0]['summary']['baseTime'])/3600,3)
-
-# api call for getting the shape of route
+# an api request that returns the shape of a route between two given waypoints as an object with many many coordinates 
 
 def route_shape(start_coord, destination_coord):
     r = requests.get('https://route.api.here.com/routing/7.2/calculateroute.json?waypoint0='+ start_coord + '&waypoint1=' + destination_coord + '&mode=fastest;car;traffic:enabled&routeattributes=shape&app_id=jccZtyShstzovgbVxAJn&app_code=5ezWDCQaAJYiXldHFRV6gA')
     route = r.json()
     return route
 
-route_shape = route_shape(start_coord,destination_coord)
-
-# get lat/ lon of route shape 
-
-lat_lon_1 = []
-
-for x in range(len(route_shape['response']['route'][0]['shape'])):
-    lat_lon = route_shape['response']['route'][0]['shape'][x]
-    lat_lon_1.append(lat_lon)
- 
-    
-# delete every second shape several times to reduce size  
-# to do : write a loop that the list isnt too small (like only 10 elements, minimum elements = 160 ? )
-    
-del lat_lon_1[::2]
-del lat_lon_1[::2]
-del lat_lon_1[::2]
-del lat_lon_1[::2]
-del lat_lon_1[::2]
-
-del lat_lon_1[0:40]
-
-    
-# convert to string
-string_route_shapes = '|'.join(lat_lon_1)
-string_route_shapes = '[' + string_route_shapes + ']'    
-
-    
-# parse shape to corridor api / is shape too big? / convert into polyline encoding of here maps
-      
-corridor_width = 1000
-size_of_results = 100
+# an api request that returns all stations along a route in specified corridor around the route
 
 def get_stations_along_route(string_route_shapes,corridor_width, size_of_results):
     r = requests.get('https://places.cit.api.here.com/places/v1/browse/by-corridor?app_id=jccZtyShstzovgbVxAJn&app_code=5ezWDCQaAJYiXldHFRV6gA&route=' +string_route_shapes + ';w=' + str(corridor_width)+ '&cat=ev-charging-station&size='+ str(size_of_results))
     stations = r.json()
     return stations
 
+def complete_route(fe_start, fe_dest, range_of_car):
+    # api call for geocoding strings into coordinates
+    start_coord = geocoder(start)
+    destination_coord = geocoder(destination)
+    list_of_waypoints = []
+    list_of_waypoints.append(start_coord)
+    # api call for route calculation
+    print("1")
+    route = here_route(start_coord,destination_coord)
+    trip_length_in_m = (route['response']['route'][0]['summary']['distance'])
+    if trip_length_in_m > range_of_car:
+        # api call for getting the shape of route
+        route_poly_shape = route_shape(start_coord,destination_coord)
+        # get lat/ lon of route shape 
+        polyline_coordinates = []
+        for x in range(len(route_poly_shape['response']['route'][0]['shape'])):
+            lat_lon = route_poly_shape['response']['route'][0]['shape'][x]
+            polyline_coordinates.append(lat_lon)
+        # delete every second shape several times to reduce size until size > 160
+        print("2")
+        while len(polyline_coordinates) > 160:
+            del polyline_coordinates[::2]
+        # convert Polyline to string for api call
+        string_route_shape = '|'.join(polyline_coordinates)
+        string_route_shape = '[' + string_route_shape + ']'
+        # set corridor width and result size then parse shape to corridor api and get the stations along the route
+        corridor_width = 2000
+        size_of_results = 200
+        stations_along_route = get_stations_along_route(string_route_shape,corridor_width, size_of_results)
+        #create list with all coordinates of the found stations
+        stations_coordinates = []
+        for x in range(len(stations_along_route['results']['items'])):
+            lat_1 = stations_along_route['results']['items'][x]['position'][0]
+            lon_1 = stations_along_route['results']['items'][x]['position'][1]
+            stations_coordinates.append(str(lat_1) + ',' + str(lon_1))
+        # calculating how many stations needed to be found during the trip. 
+        print("3")
+        loops = int(trip_length_in_m/range_of_car)
+        #select the first guess for a polyline point by dividing the list of polyline points by the rounded number of loops
+        first_polyline_point = int((len(polyline_coordinates))/loops)
+        #loop for all stations during trip
+        a = 1
+        print("4")
+        while a <= loops:
+            distance_polypoint_to_start = 0
+            # loop to find polypoint on route after range of car
+            # calculate route between start and polyline point
+            print(first_polyline_point)
+            route = here_route(start_coord,polyline_coordinates[first_polyline_point])
+            # get distance
+            route_to_polyline_point_length_in_m = (route['response']['route'][0]['summary']['distance'])
+            # raise counter by distance of route
+            distance_polypoint_to_start = route_to_polyline_point_length_in_m
+            print(distance_polypoint_to_start)
 
-stations_along_route = get_stations_along_route(string_route_shapes,corridor_width, size_of_results)
+            
+            if distance_polypoint_to_start >= range_of_car:
+                first_polyline_point = int(first_polyline_point/2)
+                distance_polypoint_to_start = 0
+                print(distance_polypoint_to_start)
 
-
-# now calculate the nearest station of a radius of the range of the car (radius from the start-coordinates)
-
-
-def coordinates_from_center(start, maximumRangeOfCar):
-    r = requests.get('https://isoline.route.cit.api.here.com/routing/7.2/calculateisoline.json?mode=fastest;car;traffic:disabled&start='+ start +'&rangetype=distance&range=' + maximumRangeOfCar + '&app_id=jccZtyShstzovgbVxAJn&app_code=5ezWDCQaAJYiXldHFRV6gA')
-    radius = r.json()
-    return radius
-
-maximumRangeOfCar = 200000
-radius = coordinates_from_center(start_coord,str(maximumRangeOfCar))
-
-
-radius_coordinates = []
-for x in range(len(radius['response']['isoline'][0]['component'][0]['shape'])):
-    radius_coordinate = radius['response']['isoline'][0]['component'][0]['shape'][x]
-    radius_coordinates.append(radius_coordinate)
+                print("if")
+                print(first_polyline_point)
+            else:
+                # raise counter for next polyline point
+                first_polyline_point += 2
+                print("else")
+                print(first_polyline_point)
+            while distance_polypoint_to_start <= range_of_car and first_polyline_point <= int(len(polyline_coordinates)):
+                route = here_route(start_coord,polyline_coordinates[first_polyline_point])
+                # get distance
+                route_to_polyline_point_length_in_m = (route['response']['route'][0]['summary']['distance'])
+                # raise counter by distance of route
+                distance_polypoint_to_start = route_to_polyline_point_length_in_m
+                first_polyline_point += 2
+                print("while")
+                print(first_polyline_point)
+                print(distance_polypoint_to_start)
+            #get the calculated polyline point
+            first_found_polypoint = polyline_coordinates[first_polyline_point]
+            # calculate distances from polyline point to all stations along route, take the nearest station
+            print("5")
+            distances_station_from_found_polypoint = []
+            for z in range(len(stations_coordinates)):
+                coords_1 = stations_coordinates[z]
+                coords_2 = first_found_polypoint
+                dist = geopy.distance.distance(coords_1, coords_2).km
+                distances_station_from_found_polypoint.append(dist)
+            stations_coordinates_with_distances_station_from_found_polypoint = pd.DataFrame({'distances_station_from_found_polypoint': distances_station_from_found_polypoint,'stations_coordinates': stations_coordinates})
+            row_of_station = stations_coordinates_with_distances_station_from_found_polypoint['distances_station_from_found_polypoint'].idxmin()
+            
+            print("6")
+            # get data from station
+            #station_openingHours = stations_along_route['results']['items'][row_of_station]['openingHours']['text']
+            station_position = str(stations_along_route['results']['items'][row_of_station]['position'][0])+ ','+ str(stations_along_route['results']['items'][row_of_station]['position'][1])
+            #station_title = stations_along_route['results']['items'][row_of_station]['title']
+            list_of_waypoints.append(station_position)
+            start_coord = station_position
+            print(first_polyline_point)
+            print(a)
+            first_polyline_point = int((int((len(polyline_coordinates))/loops))*1.5)
+            stations_coordinates_with_distances_station_from_found_polypoint = []
+            row_of_station = 0
+            a += 1
+    else: 
+        print("no need to carge during this trip. The Battery of the car will last")
+    list_of_waypoints.append(destination_coord)
+    return list_of_waypoints
     
-import geopy.distance
-distances_from_center = []
-for x in range(len(radius_coordinates)):
-    coords_1 = radius_coordinates[x]
-    coords_2 = destination_coord
-    dist = geopy.distance.distance(coords_1, coords_2).km
-    distances_from_center.append(dist)
+#################################################################################
+# TEST SECTION
+#################################################################################
 
-radius_coordinates_with_distances_from_center = pd.DataFrame(
-    {
-     'distances_from_center': distances_from_center,
-     'radius_coordinates': radius_coordinates
-    })
-
-min_dist = radius_coordinates_with_distances_from_center.loc[radius_coordinates_with_distances_from_center['distances_from_center'].idxmin()]
-first_center_spot = min_dist['radius_coordinates']
-
-
-# to do : compare first_center_spot with list of stations = nearest with geopy distance. 
-
-stations_coordinates = []
-for x in range(len(stations_along_route['results']['items'])):
-    lat_1 = stations_along_route['results']['items'][x]['position'][0]
-    lon_1 = stations_along_route['results']['items'][x]['position'][1]
-    stations_coordinates.append(str(lat_1) + ',' + str(lon_1))
-    
-distances_station_from_first_center = []
-for x in range(len(stations_coordinates)):
-    coords_1 = stations_coordinates[x]
-    coords_2 = first_center_spot
-    dist = geopy.distance.distance(coords_1, coords_2).km
-    distances_station_from_first_center.append(dist)
-
-    
-stations_coordinates_with_distances_from_center = pd.DataFrame(
-    {
-     'distances_station_from_first_center': distances_station_from_first_center,
-     'stations_coordinates': stations_coordinates
-    })
-
-min_dist = stations_coordinates_with_distances_from_center.loc[stations_coordinates_with_distances_from_center['distances_station_from_first_center'].idxmin()]
-first_station = min_dist['stations_coordinates']
-
-# input = min dist = coord and line number of first station , now get data from station. 
-
-# neuer ansatz: schleife bauen , die eine route berechnet bis zum ersten polyline punkt, checkt ob gefahrene distanz < range of car ist. 
-#  gefahrene distanz darf nicht > range sein. dann nimmt die funktion sich die koordinate von wegpunkt 200 km. 
-# loop durchläuft alle abstände zwischen koordinate und stationen along route. es wird station zurückgegeben mit kleinstem abstand zur station. 
-#wenn größer als range , dann in stationsliste eintrag x-1 (= eine station davor) auswählen == erste ladestation
-
-
-# berechne route 
-
-def route_to_polyline_point(start_coord, destination_coord):
-    r = requests.get('https://route.cit.api.here.com/routing/7.2/calculateroute.json?app_id=jccZtyShstzovgbVxAJn&app_code=5ezWDCQaAJYiXldHFRV6gA&waypoint0='+ start_coord + '&waypoint1=' + destination_coord + '&mode=fastest;car;traffic:disabled')
-    route = r.json()
-    return route
-
-
-# how long in m is the route ? 
-
-
-# while loop: 
-
-distance_polypoint_to_start = 0
-x = 0
-route_to_polyline_point_length_in_m = 0
-while distance_polypoint_to_start <= 200000:
-    #take polyline punkt 
-    route = route_to_polyline_point(start_coord,str(lat_lon_1[x]))
-    route_to_polyline_point_length_in_m = (route['response']['route'][0]['summary']['distance'])
-    distance_polypoint_to_start = route_to_polyline_point_length_in_m
-    x += 1
-    
-lat_lon_1[x]
-
-# data from first station
-
-
-station_openingHours = stations_along_route['results']['items'][x]['openingHours']['text']
-station_position = str(stations_along_route['results']['items'][x]['position'][0])+ ','+ str(stations_along_route['results']['items'][x]['position'][1])
-station_title = stations_along_route['results']['items'][x]['title']
-
-
-
-
-# to do:   check if distance from 1. station to destination is greater than range of car ! 
-# if yes , search next station after the first station! 
-# loop ! 
-
-
-
-# for visualization: 
-# https://developer.here.com/api-explorer/geovisualization/technology_markers/markers-csv-provider
-
-# to do: 
-
-#build some loops
-#integrate security puffer 
-#integrate in temperature
-#integrate battery level at start
-
-temparature = 20.0
-battery_level_at_start = 0.8
-security_puffer_in_km = 50 
-
-
-# to do : 
-
-#put all input values to top !
-
-
+start = 'Berlin ,Weserstrasse 12'
+destination = 'Schlossplatz+Stuttgart'
+range_of_car = 200000
+list_of_waypoints = complete_route(start, destination, range_of_car)
--- temp.py
+++ temp.py
@@ -6,51 +6,22 @@ Created on Wed May 23 10:54:30 2018
 @author: jan-lukaspflaum
 """
 
+# neuer ansatz: schleife bauen , die eine route berechnet bis zum ersten polyline punkt, checkt ob gefahrene distanz < range of car ist. 
+#  gefahrene distanz darf nicht > range sein. dann nimmt die funktion sich die koordinate von wegpunkt 200 km. 
+# loop durchläuft alle abstände zwischen koordinate und stationen along route. es wird station zurückgegeben mit kleinstem abstand zur station. 
+#wenn größer als range , dann in stationsliste eintrag x-1 (= eine station davor) auswählen == erste ladestation
 
 
-waypoints_0 = []
-lat_wp = []
-lon_wp = []
+# for visualization: 
+# https://developer.here.com/api-explorer/geovisualization/technology_markers/markers-csv-provider
 
+# to do: 
 
-for x in range(len(route['response']['route'][0]['leg'][0]['maneuver'])):
-    wp = route['response']['route'][0]['leg'][0]['maneuver'][x]['length']
-    lat = route['response']['route'][0]['leg'][0]['maneuver'][x]['position']['latitude']
-    lon = route['response']['route'][0]['leg'][0]['maneuver'][x]['position']['longitude']
-    waypoints_0.append(wp)
-    lat_wp.append(lat)
-    lon_wp.append(lon)
-#
-#
-waypoints_with_coordinates = pd.DataFrame(
-    {
-     'lat_wp': lat_wp,
-     'lon_wp': lon_wp
-    })
-   
-    
-    ######################
-    
-    
-    def stations_near_center(coord):
-    r = requests.get('https://places.cit.api.here.com/places/v1/browse?at='+ coord +'&cat=ev-charging-station&app_id=jccZtyShstzovgbVxAJn&app_code=5ezWDCQaAJYiXldHFRV6gA')
-    stations = r.json()
-    return stations
+#integrate security puffer 
 
-######################
-    
+#integrate in temperature
+#integrate battery level at start
 
-i = 0 
-dist = 0 
-
-while dist <= maximumRangeOfCar - waypoints_with_coordinates['waypoints'][i+1]:
-    dist = dist + waypoints_with_coordinates['waypoints'][i]
-    i += 1
-    
-first_stop_lat = waypoints_with_coordinates['lat_wp'][i]
-first_stop_lon = waypoints_with_coordinates['lon_wp'][i]
-
-
-
-
-
+temparature = 20.0
+battery_level_at_start = 0.8
+security_puffer_in_km = 50 
