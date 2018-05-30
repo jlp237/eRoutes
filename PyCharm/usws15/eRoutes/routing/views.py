269a0a784311523a3131ad3b6305200d8d422817
DavidDanielSteiner <38961224+DavidDanielSteiner@users.noreply.github.com>
2018-05-30 22:34:38 +0200

    Mucho update

--- PyCharm/usws15/eRoutes/routing/views.py
+++ PyCharm/usws15/eRoutes/routing/views.py
@@ -51,6 +51,8 @@ def my_view(request):
     render(request, 'routing/team.html', context)
 
 
+
+###### OUTPUT #######
 def output(request):
     if request.method == 'POST':
         start = request.POST.get('start', 'leer')
@@ -59,14 +61,39 @@ def output(request):
         driving_style = request.POST.get('driving_style', 'leer')
         battery_status = request.POST.get('battery_status', 'leer')
 
-        distance = get_direction_data(start, destination)
+
+        route_json = get_direction_data(start, destination)
+
+        # Waypoints string
+        waypoints = route_json['waypoints_return']
+
+        # Total Distance
+        distance_array = []
+        for i in range(len(route_json['routes'][0]['legs'])):
+            charging_station = route_json['routes'][0]['legs'][i]['distance']['value']
+            distance_array.append(charging_station)
+        distance = sum(distance_array)
+
+        # Driving Time
+        driving_time_array = []
+        for i in range(len(route_json['routes'][0]['legs'])):
+            charging_station = route_json['routes'][0]['legs'][i]['duration']['value']
+            driving_time_array.append(charging_station)
+        driving_time = round(sum(driving_time_array) / 3600, 2)
+
+
+
+        #distance = direction_json['legs_distance']
+        #waypoints = waypoints_x['legs_distance']
+
+        #resp_dict = json.loads(resp_str)
+
         temperature_start = get_weather_start(start)
         temperature_destination = get_weather_destination(destination)
         geo_coordinates = get_geo_data(start)
 
         total_cost = round((distance / 1000) * 1.6 * 0.025, 2)
         money_saved = round(((distance / 1000) * 1.6 * 0.13) - total_cost, 2)
-
         distance = round(distance / 1000, 1)
 
         return render(request, 'routing/output.html', {'start': start,
@@ -80,11 +107,13 @@ def output(request):
                                                        'distance': distance,
                                                        'total_cost': total_cost,
                                                        'money_saved': money_saved,
+                                                       'waypoints': waypoints,
+                                                       'driving_time': driving_time,
                                                        })
     else:
         return render(request, 'routing/output.html')
 
-
+######################## APIs #######################
 def get_weather_start(start):
     api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=31d7b827392ab249e871954306d44d04&q='
     url = api_address + start
@@ -92,6 +121,7 @@ def get_weather_start(start):
     kelvin = json_data['main']['temp']
     temperature_start = round(kelvin - 273.15, 0)
     return temperature_start
+
 
 def get_weather_destination(destination):
     api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=31d7b827392ab249e871954306d44d04&q='
@@ -111,17 +141,38 @@ def get_geo_data(start):
     return geo_data
 
 
+#
 def get_direction_data(start, destination):
     api_key = 'AIzaSyAonN5q0C_6Vlvm8VGIWPd-vl43vjJqca0'
     endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-    nav_request = 'origin={}&destination={}&key={}'.format(start, destination, api_key)
+    waypoint_array = ["Frankfurt", "München", "Paris"]
+
+    # create waypoints strings
+    waypoint_counter = 1
+    waypoint_map_string = ""
+    waypoint_request_string = ""
+    for waypoint in waypoint_array:
+        waypoint_map_string += 'location' + str(waypoint_counter) + '=' + waypoint + '&'
+        waypoint_request_string += waypoint + '|'
+        waypoint_counter += 1
+    waypoints_return = waypoint_map_string[:-1]
+    waypoints = waypoint_request_string[:-1]
+
+    # make request for routing json
+    nav_request = 'origin={}&destination={}&waypoints={}&key={}'.format(start, destination, waypoints, api_key)
     url = endpoint + nav_request
     json_data = requests.get(url).json()
-
     routes = json_data['routes']
-    legs = routes[0]['legs']
-    legs_distance = legs[0]['distance']['value']
-    return legs_distance
+
+    # prepare return json
+    data = {
+        'routes': routes,
+        'waypoints_return': waypoints_return,
+    }
+    return data
+
+
+######################## AJAX HANDLER #######################
 
 
 def get_car_data(request):
@@ -148,3 +199,5 @@ def get_car_data(request):
     else:
         return HttpResponse("ERROR get_car_data")
 
+
+######################## ALGORITHM #######################
